096bfcdf10128c82cffde18c5129e8161a775f95
BenVosper <ben-vosper@hotmail.com>
2024-10-31 23:48:12 +0000

    Implement autoscroll
    This smooth scrolling library implements the scroll momentum and makes
    clicking the anchor nav elements feel way better. All works very nicely.
    https://github.com/monsieurdd/mrD-SmoothScroller
    
    The only nice to have would be some momentum on the click and drag
    release of the imagery elements. Which is probably doable with a
    requestAnimationFrame kinda thing and the right decelleration function.

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,5 +1,59 @@
 import { throttle } from "./utils.js";
 
+let isAutoScrolling = true;
+
+// ==== Settings ====
+const autoScrollPixelsPerSecond = 100;
+const autoScrollFPS = 60;
+// ==================
+
+const autoScrollPeriod = 1000 / autoScrollFPS;
+const autoScrollPixelsPerFrame = autoScrollPixelsPerSecond / autoScrollFPS;
+
+const autoScroll = () => {
+  if (!isAutoScrolling) {
+    return;
+  }
+  window.scrollBy(0, autoScrollPixelsPerFrame);
+};
+
+const initImageryElement = (el) => {
+  let isDown = false;
+  let lastJump;
+
+  const imageryMouseDown = () => {
+    isDown = true;
+    el.style.cursor = "grabbing";
+  };
+  const imageryMouseUp = () => {
+    isDown = false;
+    el.style.cursor = "grab";
+  };
+  const imageryMouseEnter = () => {
+    isAutoScrolling = false;
+  };
+  const imageryMouseLeave = () => {
+    isAutoScrolling = true;
+    isDown = false;
+    el.style.cursor = "grab";
+  };
+  const imageryMouseMove = (event) => {
+    if (!isDown) {
+      return;
+    }
+    let moveY = event.movementY;
+    lastJump = moveY;
+    window.scrollBy(0, -moveY);
+  };
+
+  el.addEventListener("mousedown", imageryMouseDown);
+  el.addEventListener("mouseup", imageryMouseUp);
+  el.addEventListener("mouseover", imageryMouseEnter);
+  el.addEventListener("mouseout", imageryMouseLeave);
+  el.addEventListener("mousemove", imageryMouseMove);
+  el.ondragstart = () => false;
+};
+
 const onScroll = () => {
   const projects = [...document.querySelectorAll("article")];
   const distancesToCenterOfViewport = projects.map((element) => {
@@ -22,5 +76,9 @@ const onScroll = () => {
 document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("scroll", throttle(onScroll, 100));
   onScroll(); // Call once to set initial value
+
+  [...document.querySelectorAll("div.imagery")].forEach(initImageryElement);
+
+  setInterval(autoScroll, autoScrollPeriod);
   console.debug("projects init complete");
 });
