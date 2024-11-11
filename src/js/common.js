c0a0d5da42630afc611c990b63efb3f0b2137c85
BenVosper <ben-vosper@hotmail.com>
2024-11-11 21:40:26 +0000

    Refactor buttons click handlers

--- src/js/common.js
+++ src/js/common.js
@@ -20,80 +20,123 @@ const initElements = () => {
 
 export const isMobile = window.matchMedia("(orientation: portrait)");
 
-const onProjectsClick = (event) => {
-  if (isMobile.matches) {
-    if (elements.projectsButton.classList.contains("secondary")) {
-      elements.projectsButton.classList.add("primary");
-      elements.projectsButton.classList.remove("secondary");
-      elements.contactButton.classList.remove("primary");
-      elements.contactButton.classList.add("secondary");
-      elements.aboutButton.classList.remove("primary");
-      elements.aboutButton.classList.add("secondary");
-      fadeIn(elements.nav);
-      fadeOut(elements.about);
-      fadeOut(elements.contact);
-    }
-    event.preventDefault();
+const m_activateProjectsTab = () => {
+  elements.projectsButton.classList.add("primary");
+  elements.projectsButton.classList.remove("secondary");
+  fadeIn(elements.nav);
+};
+
+const m_deactivateProjectsTab = () => {
+  elements.projectsButton.classList.remove("primary");
+  elements.projectsButton.classList.add("secondary");
+  fadeOut(elements.nav);
+};
+
+const m_activateAboutTab = () => {
+  elements.aboutButton.classList.add("primary");
+  elements.aboutButton.classList.remove("secondary");
+  fadeIn(elements.about);
+};
+
+const m_deactivateAboutTab = () => {
+  elements.aboutButton.classList.remove("primary");
+  elements.aboutButton.classList.add("secondary");
+  fadeOut(elements.about);
+};
+
+const m_activateContactTab = () => {
+  elements.contactButton.classList.add("primary");
+  elements.contactButton.classList.remove("secondary");
+  fadeIn(elements.contact);
+};
+
+const m_deactivateContactTab = () => {
+  elements.contactButton.classList.remove("primary");
+  elements.contactButton.classList.add("secondary");
+  fadeOut(elements.contact);
+};
+
+const m_onProjectsClick = (event) => {
+  const isActive = !elements.projectsButton.classList.contains("secondary");
+  if (!isActive) {
+    m_activateProjectsTab();
+    m_deactivateAboutTab();
+    m_deactivateContactTab();
+  }
+  event.preventDefault();
+};
+
+const m_onAboutClick = () => {
+  const isActive = !elements.aboutButton.classList.contains("secondary");
+  if (!isActive) {
+    m_activateAboutTab();
+    m_deactivateProjectsTab();
+    m_deactivateContactTab();
   }
 };
 
-const onAboutClick = () => {
-  if (isMobile.matches) {
-    if (elements.aboutButton.classList.contains("secondary")) {
-      elements.projectsButton.classList.remove("primary");
-      elements.projectsButton.classList.add("secondary");
-      elements.contactButton.classList.remove("primary");
-      elements.contactButton.classList.add("secondary");
-      elements.aboutButton.classList.add("primary");
-      elements.aboutButton.classList.remove("secondary");
-      fadeIn(elements.about);
-      fadeOut(elements.contact);
-      fadeOut(elements.nav);
-    }
-    return;
-  }
-  if (elements.aboutButton.classList.contains("open")) {
-    elements.aboutButton.classList.remove("open");
-    elements.aboutButton.textContent = "About";
-    fadeOut(elements.about);
-  } else {
-    elements.aboutButton.classList.add("open");
-    elements.aboutButton.textContent = "Close";
-    fadeIn(elements.about);
+const m_onContactClick = () => {
+  const isActive = !elements.contactButton.classList.contains("secondary");
+  if (!isActive) {
+    m_activateContactTab();
+    m_deactivateProjectsTab();
+    m_deactivateAboutTab();
   }
 };
 
-const onContactClick = () => {
-  if (isMobile.matches) {
-    if (elements.contactButton.classList.contains("secondary")) {
-      elements.projectsButton.classList.remove("primary");
-      elements.projectsButton.classList.add("secondary");
-      elements.aboutButton.classList.remove("primary");
-      elements.aboutButton.classList.add("secondary");
-      elements.contactButton.classList.add("primary");
-      elements.contactButton.classList.remove("secondary");
-      elements.aboutButton.classList.add("secondary");
-      fadeIn(elements.contact);
-      fadeOut(elements.about);
-      fadeOut(elements.nav);
-    }
-    return;
-  }
-  if (elements.contactButton.classList.contains("open")) {
-    elements.contactButton.classList.remove("open");
-    elements.contactButton.textContent = "Contact";
-    fadeOut(elements.contact);
+const d_openAbout = () => {
+  elements.aboutButton.classList.add("open");
+  elements.aboutButton.textContent = "Close";
+  fadeIn(elements.about);
+};
+
+const d_closeAbout = () => {
+  elements.aboutButton.classList.remove("open");
+  elements.aboutButton.textContent = "About";
+  fadeOut(elements.about);
+};
+
+const d_onAboutClick = () => {
+  const isOpen = elements.aboutButton.classList.contains("open");
+  if (isOpen) {
+    d_closeAbout();
   } else {
-    elements.contactButton.classList.add("open");
-    elements.contactButton.textContent = "Close";
-    fadeIn(elements.contact);
+    d_openAbout();
+  }
+};
+
+const d_openContact = () => {
+  elements.contactButton.classList.add("open");
+  elements.contactButton.textContent = "Close";
+  fadeIn(elements.contact);
+};
+
+const d_closeContact = () => {
+  elements.contactButton.classList.remove("open");
+  elements.contactButton.textContent = "Contact";
+  fadeOut(elements.contact);
+};
+
+const d_onContactClick = () => {
+  const isOpen = elements.contactButton.classList.contains("open");
+  if (isOpen) {
+    d_closeContact();
+  } else {
+    d_openContact();
   }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   initElements();
-  elements.projectsButton.addEventListener("click", onProjectsClick);
-  elements.aboutButton.addEventListener("click", onAboutClick);
-  elements.contactButton.addEventListener("click", onContactClick);
+
+  if (isMobile.matches) {
+    elements.projectsButton.addEventListener("click", m_onProjectsClick);
+    elements.aboutButton.addEventListener("click", m_onAboutClick);
+    elements.contactButton.addEventListener("click", m_onContactClick);
+  } else {
+    elements.aboutButton.addEventListener("click", d_onAboutClick);
+    elements.contactButton.addEventListener("click", d_onContactClick);
+  }
+
   console.debug("common init complete");
 });
